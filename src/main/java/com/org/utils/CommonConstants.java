1aa653b5e3c70d8fc32ab889319c922f44dad65e
j <namdev.arade@gmail.com>
2017-06-09 23:35:10 +0530

    comit

--- src/main/java/com/org/utils/CommonConstants.java
+++ src/main/java/com/org/utils/CommonConstants.java
@@ -4,8 +4,7 @@ public class CommonConstants {
 	
 	public static final String PGR_ISSUNEW_HEADER = "plant_loc,Pgr_no,Rack_Code,Shelf_Code,block_code,pi_Branchcode,pi_Variety,pi_CorB,pi_Opcode,pi_Stage,pi_Week,pi_Day,pi_Shift,pi_Inoculation,pi_Bottles";
 	public static final String CONTAMINATION_HEADER = "cn_Plantloc,Pgr_no,Rack_Code,Shelf_Code,Block_Code,cn_Branchcode,cn_Variety,cn_Corb,cn_Opcode,cn_Stage,cn_Week,cn_Day,cn_Shift,cn_Inoculation,cn_Nature,cn_Date,cn_Time,cn_prweek,cn_prday,cn_pryear,cn_Year";
-	public static final String PGR_PRODUCTION_HEADER = "plant_loc,Pgr_No,Rack_Code,Shelf_Code,block_code,pr_BranchCode,cr_Variety,cr_CorB,cr_Opcode,cr_Stage,cr_Week,cr_Day,cr_Shift,cr_Inoculation,cr_Bottles";
-	
+		
 	public static final String TEXTDATA_HEADER = null;
 	
 	public static final String KF_SCUCESS="1";
